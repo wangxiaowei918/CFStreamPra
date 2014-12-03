204e2c2d10712a30065a81cdcc9eb83d6d031113
Tom&Jerry <pangdongming@pangdongmings-MacBook-Air.local>
2014-12-03 23:45:37 +0800

    1
    Socket ,stream

--- CFStreamPra/ViewController.m
+++ CFStreamPra/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    CFSocketRef
+//    CFStreamCreatePairWithSocketToHost(CFAllocatorRef alloc, CFStringRef host, UInt32 port, CFReadStreamRef *readStream, CFWriteStreamRef *writeStream)
 }
 
 - (void)didReceiveMemoryWarning {
